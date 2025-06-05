61d3912ccda50d1bc987285970e22a95e92f3211
karlis-vagalis <karlis.vagalis@gmail.com>
2025-06-05 16:51:43 +0200

    Readded path absolutization

--- tests/cli_current.rs
+++ tests/cli_current.rs
@@ -7,10 +7,11 @@ use predicates::prelude::*;
 #[test]
 fn test_current_no_git_repo() {
     let temp_dir = tempfile::tempdir().unwrap();
-    let cmd = Command::cargo_bin("doxxer")
+    Command::cargo_bin(env!("CARGO_PKG_NAME"))
         .unwrap()
-        .current_dir(temp_dir)
+        .current_dir(temp_dir.path())
         .arg("current")
         .assert()
-        .failure();
+        .failure()
+        .stderr(predicate::str::contains(format!("Issue opening repository: could not find repository at '{}'!", temp_dir.path().to_str().unwrap())));
 }
